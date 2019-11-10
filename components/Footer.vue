a59891037ceaab3c0c2ec5c0d9a0e8600c5c8039
to4-yanagi <yanagisawa@laughcraft.dev>
2019-11-10 17:16:17 +0900

    footerの実装

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="footer">
+    <p>© 2019 Toshiaki Yanagisawa</p>
+  </article>
+</template>
+
+<style lang="scss">
+.footer {
+  font-size: 0.8rem;
+  padding: $basic-half-margin;
+  border-top: 1px solid $color-basic-border;
+  background-color: $color-basic-bg;
+  text-align: center;
+}
+</style>
