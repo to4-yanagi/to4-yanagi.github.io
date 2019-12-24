c08251cf92e49f2a3281fc3a0346f2a820819382
to4-yanagi <yanagisawa@laughcraft.dev>
2019-12-24 16:40:25 +0900

    5b1ed1d

---
+++ mixin.js
@@ -0,0 +1,10 @@
+export default {
+  data () {
+    return {
+      siteTitle: `Yanagi's portfolio`,
+      mail: 'yanagisawa@laughcraft.dev',
+      twitterLink: 'https://twitter.com/to4_yanagi',
+      githubLink: 'https://github.com/to4-yanagi',
+    }
+  }
+}
